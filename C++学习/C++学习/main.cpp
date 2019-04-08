e5b40da2ac46ffa73465c44f622378d23b300dce
chenzhen <fhwu_chen@163.com>
2019-04-08 21:10:03 +0800

    hello world

---
+++ C++学习/C++学习/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  C++学习
+//
+//  Created by 陈真 on 2019/4/8.
+//  Copyright © 2019 cz. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
